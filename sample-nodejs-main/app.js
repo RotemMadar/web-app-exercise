199cb356f03f935240e6d254574f300c8b752f96
RotemMadar <rotemmadar9@gmail.com>
2026-06-12 15:57:25 +0300

    Changed source code

--- sample-nodejs-main/app.js
+++ sample-nodejs-main/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const promClient = require('prom-client');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8083;
 
 // Create a Registry to register the metrics
 const register = new promClient.Registry();
